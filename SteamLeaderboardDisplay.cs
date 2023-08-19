843831b354e698dcf91b5d71df890b14d8a3e3a7
Robert Rumney <robertrumney@gmail.com>
2023-08-19 22:03:51 +0200

    Update SteamLeaderboardDisplay.cs

--- SteamLeaderboardDisplay.cs
+++ SteamLeaderboardDisplay.cs
@@ -4,18 +4,20 @@ using UnityEngine.UI;
 
 public class SteamLeaderboardDisplay : MonoBehaviour
 {
+    public bool getScores = true;
+    
+    public Text info;
+    public Text[] scores;
+
+    [HideInInspector]
     public SteamLeaderboardEntries_t m_SteamLeaderboardEntries;
-    public bool getScores;
-    public Text scores;
-    public Text[] actualScores;
     private static readonly CallResult<LeaderboardScoresDownloaded_t> m_scoresDownloadedResult = new CallResult<LeaderboardScoresDownloaded_t>();
 
     private void OnEnable()
     {
-        if (scores)
-        {
-            scores.text = "LOADING SCORES...";
-        }
+        if (info)
+            info.text = "LOADING SCORES...";
+        
         if (getScores)
             GetScores();
     }
@@ -42,13 +44,13 @@ public class SteamLeaderboardDisplay : MonoBehaviour
 
     private void ProcessDownloadedScores(LeaderboardScoresDownloaded_t pCallback)
     {
-        scores.gameObject.SetActive(false);
+        info.gameObject.SetActive(false);
 
         // Reset text fields
-        foreach (Text actualScore in actualScores)
+        foreach (Text score in scores)
         {
-            actualScore.text = "";
-            actualScore.gameObject.SetActive(true);
+            score.text = "";
+            score.gameObject.SetActive(true);
         }
 
         int numEntries = pCallback.m_cEntryCount;
@@ -63,17 +65,18 @@ public class SteamLeaderboardDisplay : MonoBehaviour
             SteamUserStats.GetDownloadedLeaderboardEntry(pCallback.m_hSteamLeaderboardEntries, index, out leaderboardEntry, null, 0);
             string username = SteamFriends.GetFriendPersonaName(leaderboardEntry.m_steamIDUser);
 
-            // Handle unknown usernames
+            // Handle rare unknown username issue and reset leaderboard
             if (username.ToUpper() == "[UNKNOWN]")
             {
-                if (scores)
+                if (info)
                 {
                     // Display loading message
-                    scores.gameObject.SetActive(true);
-                    scores.text = "LOADING SCORES...";
-                    foreach (Text actualScore in actualScores)
+                    info.gameObject.SetActive(true);
+                    info.text = "LOADING SCORES...";
+                    
+                    foreach (Text score in scores)
                     {
-                        actualScore.gameObject.SetActive(false);
+                        score.gameObject.SetActive(false);
                     }
                 }
 
@@ -81,28 +84,28 @@ public class SteamLeaderboardDisplay : MonoBehaviour
                 return;
             }
 
-            // Display leaderboard entry in the appropriate UI element based on rank
+            // Display leaderboard entry in the appropriate UI element based on rank - here we use 4 columns to fit up to 100 scores with 33 scores in each column, and a single score in column one, adjust to your needs.
             if (rank == 1)
             {
-                actualScores[0].text += "#" + rank.ToString() + ". " + username.ToUpper() + "  : <color=#E50000> " + leaderboardEntry.m_nScore.ToString("n2") + "</color>\n";
+                scores[0].text += "#" + rank.ToString() + ". " + username.ToUpper() + "  : <color=#E50000> " + leaderboardEntry.m_nScore.ToString("n2") + "</color>\n";
             }
             else if (rank > 1 && rank < 35)
             {
-                actualScores[1].text += "#" + rank.ToString() + ". " + username.ToUpper() + "  : <color=#E50000> " + leaderboardEntry.m_nScore.ToString("n2") + "</color>\n";
+                scores[1].text += "#" + rank.ToString() + ". " + username.ToUpper() + "  : <color=#E50000> " + leaderboardEntry.m_nScore.ToString("n2") + "</color>\n";
             }
             else if (rank > 34 && rank < 68)
             {
-                actualScores[2].text += "#" + rank.ToString() + ". " + username.ToUpper() + "  : <color=#E50000> " + leaderboardEntry.m_nScore.ToString("n2") + "</color>\n";
+                scores[2].text += "#" + rank.ToString() + ". " + username.ToUpper() + "  : <color=#E50000> " + leaderboardEntry.m_nScore.ToString("n2") + "</color>\n";
             }
             else
             {
-                actualScores[3].text += "#" + rank.ToString() + ". " + username.ToUpper() + "  : <color=#E50000> " + leaderboardEntry.m_nScore.ToString("n2") + "</color>\n";
+                scores[3].text += "#" + rank.ToString() + ". " + username.ToUpper() + "  : <color=#E50000> " + leaderboardEntry.m_nScore.ToString("n2") + "</color>\n";
             }
 
             rank++;
         }
 
-        // Update the "scores" text field with additional information
-        scores.text += "\n\nPRESS ANY KEY TO RETURN";
+        // Update the "info" text field with additional information
+        info.text += "\n\nPRESS ANY KEY TO RETURN";
     }
 }
